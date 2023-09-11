707db2fcbd46db6455c620a698f224aa73b6e874
akayola <akayola@gmail.com>
2023-09-11 16:34:56 -0400

    again

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<p><b>This is New Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-        <h2>TIME FOR A COLD BEER? I DON'T DRINK!!!</h2>
-        <h2>I DON'T DRINK!!!</h2>
+        <h2>TIME FOR A COLD BEER?</h2>
+        <h2>I DON'T HAVE TIME, AND I DON'T DRINK!!!</h2>
 	</body> 
 </html>
